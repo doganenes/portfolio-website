6a915b1cef62bb50032a1d475f7e0b3f1f75f3ff
Enes Doğan <enesdoganlive@hotmail.com>
2023-07-08 19:54:57 +0300

    Projects blade modified

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,60 +1,65 @@
-<div id="projects" class="row content content-2 h-100 pt-4 pb-5">
+<div id="projects" class="row content content-2 h-100 pt-4 mt-5 pb-5">
     <h1 class="title-card mt-5 pb-2 text-center fw-bold fs-1">
         Some of my projects:
     </h1>
-    <div class="col-sm-4">
-        <div class="card card-1 cursor-pointer mt-3 rounded">
-            <div class="card-header">
-                <img class="img-fluid img-thumbnail" src="{{ asset('img/img1.png') }}" alt="Todo App" />
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card cursor-pointer rounded">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img1.png') }}" alt="Todo App" />
+                </div>
+                <div class="card-body text-center">Todo App</div>
             </div>
-            <div class="card-body text-center">Todo App</div>
         </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="card card-2 cursor-pointer mt-3">
-            <div class="card-header">
-                <img class="img-fluid" src="{{ asset('img/img2.png') }}" alt="Medium Clone" />
+        <div class="col-sm-4">
+            <div class="card cursor-pointer">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img2.png') }}" alt="Medium Clone" />
+                </div>
+                <div class="card-body text-center">Medium Clone</div>
             </div>
-            <div class="card-body text-center">Medium Clone</div>
         </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="card card-3 mt-3 cursor-pointer rounded">
-            <div class="card-header">
-                <img class="img-fluid img-thumbnail" src="{{ asset('img/img3.png') }}" alt="Weather App" />
-            </div>
-            <div class="card-body text-center">Weather App</div>
-        </div>
-    </div>
-</div>
-<div class="row content content-2 h-100" id="projects">
-    <div class="col-sm-4">
-        <div class="card card-4 mt-3 cursor-pointer rounded">
-            <div class="card-header">
-                <img class="img-fluid img-thumbnail" src="{{ asset('img/img4.png') }}" alt="Personal Portfolio" />
-            </div>
-            <div class="card-body text-center">Personal Portfolio</div>
-        </div>
-    </div>
-    <div class="col-sm-4">
-        <div class="card card-5 mt-3 cursor-pointer rounded">
-            <div class="card-header">
-                <img class="img-fluid img-thumbnail" src="{{ asset('img/img5.png') }}" alt="Quiz App" />
-            </div>
-            <div class="card-body text-center">
-                Quiz App
+        <div class="col-sm-4">
+            <div class="card cursor-pointer rounded">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img3.png') }}" alt="Weather App" />
+                </div>
+                <div class="card-body text-center">Weather App</div>
             </div>
         </div>
     </div>
-    <div class="col-sm-4">
-        <div class="card mt-3 card-6 cursor-pointer">
-            <div class="card-header">
-                <img class="img-fluid img-thumbnail" src="{{ asset('img/img6.jpeg') }}" alt="Filter Search App" />
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card cursor-pointer rounded">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img4.png') }}" alt="Personal Portfolio" />
+                </div>
+                <div class="card-body text-center">Personal Portfolio</div>
             </div>
-            <div class="card-body text-center">Filter Search App</div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card cursor-pointer rounded">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img5.png') }}" alt="Quiz App" />
+                </div>
+                <div class="card-body text-center">
+                    Quiz App
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card cursor-pointer">
+                <div class="card-header">
+                    <img class="img-fluid img-thumbnail" src="{{ asset('img/img6.jpeg') }}" alt="Filter Search App" />
+                </div>
+                <div class="card-body text-center">Filter Search App</div>
+            </div>
         </div>
     </div>
-    <div class="col-12 text-center pt-4 mt-2">
-        <a href="https://github.com/doganenes" target="_blank" class="btn btn-lg showAllBtn">Show All <span>></span></a>
+    <div class="row">
+        <div class="col-12 text-center pt-4 mt-2">
+            <a href="https://github.com/doganenes" target="_blank" class="btn btn-lg showAllBtn">Show All
+                <span>></span></a>
+        </div>
     </div>
 </div>
